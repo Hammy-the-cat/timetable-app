47af720c2e53048aa9cb787a7633433b46b82890
Hammy the cat <excitedcherry@yahoo.co.jp>
2026-01-11 08:19:25 +0900

    Try to fix build output visibility on GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/timetable-app',
-  assetPrefix: '/timetable-app',
   images: {
     unoptimized: true,
   },
-  distDir: 'out',
   trailingSlash: true,
   // @ts-ignore
   eslint: {
